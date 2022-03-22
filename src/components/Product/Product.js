1edbd23660f7bd11d593a216c37bb421b84af389
codewithshamim <shamimislamonline@gmail.com>
2022-03-22 17:57:27 +0600

    added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {img,name} = props.product;
+    const {img,name,price,seller,ratings} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
             <p className='product-name'>{name}</p>
+            <p className='product-price'>Price: {price}</p>
+            <div className='product-info'>
+                <p>Seller: {seller}</p>
+                <p>Ratings: {ratings}</p>
+            </div>
         </div>
     );
 };
