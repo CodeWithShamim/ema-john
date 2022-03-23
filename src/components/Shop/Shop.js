9c154b334352c838f9da8ab12af5d5496d55ea33
codewithshamim <shamimislamonline@gmail.com>
2022-03-23 16:56:38 +0600

    update

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -21,13 +21,13 @@ const Shop = () => {
 
     
     return (
-        <div className='shop-container row'>
-            <div className="products-container col-md-8">
+        <div className='shop-container p-4 row'>
+            <div className="products-container col-md-8 order-2 order-md-1">
                 <div className='row gy-5'>
                      {products.map(product=><Product key={product.id} product={product} addToCart={addToCart}></Product>)}
                 </div>
             </div>
-            <div className="cart-container col-md-4">
+            <div className="cart-container col-md-4 order-1 order-md-2">
                 <div className="sticky-item">
                     <Cart cart={cart}></Cart>
                 </div>
