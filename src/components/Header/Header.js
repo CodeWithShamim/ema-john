a3d028bda7793cca427d7e2af724547fc8b593c4
codewithshamim <shamimislamonline@gmail.com>
2022-03-24 13:24:13 +0600

    udpate

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header=() => {
         <div className='row header-sticky'>
             <nav className='header-nav col-md-12 header-sticky'>
                 <a href="https://ema-john-shamim.netlify.app/"><img src={logo} alt="" /></a>
-                <div className='me-5'>
+                <div className='me-5 header-info'>
                     <a href="https://ema-john-shamim.netlify.app/">Order</a>
                     <a href="https://ema-john-shamim.netlify.app/">Order Review</a>
                     <a href="https://ema-john-shamim.netlify.app/">Manage Inventory</a>
