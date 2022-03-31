85f1ca9de1418410f1f8fd5b35c93848a862b3f4
codewithshamim <shamimislamonline@gmail.com>
2022-03-31 19:12:49 +0600

    Done

--- src/components/ManageInventory/ManageInventory.js
+++ src/components/ManageInventory/ManageInventory.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ManageInventory = () => {
     return (
-        <div>
-            <h1>this is ManageInventory</h1>
+        <div className='d-flex justify-content-center mt-5'>
+            <h1 className='text-primary'>	&#128516; Comming Soon..........</h1>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,11 @@ import React from 'react';
 const NotFound=() => {
     return (
         <div className='d-flex align-items-center justify-content-center mt-5'>
-            <p className='fs-1 fw-semibold'>404 <span className='text-danger'>|</span> Nothing here to see</p>
+            <p className='fs-1 fw-semibold'>404 <span className='text-danger'>|</span> Nothing here to see
+            <h1 className='fs-1 text-center p-5'>
+            &#128516;
+            </h1>
+            </p>
         </div>
     );
 };
