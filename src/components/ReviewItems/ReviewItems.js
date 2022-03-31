8c560a6923be6d57b60502831023e8813e273383
codewithshamim <shamimislamonline@gmail.com>
2022-03-31 14:56:27 +0600

    review page done

--- src/components/ReviewItems/ReviewItems.js
+++ src/components/ReviewItems/ReviewItems.js
@@ -1,15 +1,23 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './ReviewItems.css'
 
 const ReviewItems = ({item}) => {
-    const {img, price, shipping, quantity} = item;
+    const {img, name, price, shipping} = item;
     console.log(item);
     return (
-        <div className='container bg-light col-md-8 mt-4 rounded d-flex justify-content-evenly align-items-center'>
+        <div className='container bg-light col-md-8 mt-4 rounded d-flex justify-content-evenly align-items-center border border-warning'>
             <img id='custom-img' src={img} className='img-fluid' alt="" />
-            <h1>{price}</h1>
-            <h1>{shipping}</h1>
-            <h1>{quantity}</h1>
+           <div className='p-4'>
+                <h4>{name}</h4>
+                <h6>Price: <span className='text-warning'>${price}</span></h6>
+                <h6>Shipping Charge: <span className='text-warning'>${shipping}</span></h6>
+           </div>
+           <div className='icon-container'>
+               <FontAwesomeIcon id='custom-icon' className='text-danger fs-2 p-3' icon={faTrashAlt}></FontAwesomeIcon>
+           </div>
+            
             
         </div>
     );
