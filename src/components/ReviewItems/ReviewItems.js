4f06db1d98fa5bfad08aa97444ad79172318e5f9
codewithshamim <shamimislamonline@gmail.com>
2022-03-31 14:27:20 +0600

    review page style added

--- src/components/ReviewItems/ReviewItems.js
+++ src/components/ReviewItems/ReviewItems.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './ReviewItems.css'
 
 const ReviewItems = ({item}) => {
     const {img, price, shipping, quantity} = item;
     console.log(item);
     return (
-        <div className='container'>
-            <img src={img} className='img-fluid' alt="" />
+        <div className='container bg-light col-md-8 mt-4 rounded d-flex justify-content-evenly align-items-center'>
+            <img id='custom-img' src={img} className='img-fluid' alt="" />
+            <h1>{price}</h1>
+            <h1>{shipping}</h1>
+            <h1>{quantity}</h1>
+            
         </div>
     );
 };
