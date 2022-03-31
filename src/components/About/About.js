88a9c902dc982386def71981317697cb2f82ff56
codewithshamim <shamimislamonline@gmail.com>
2022-03-31 11:35:06 +0600

    about added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>This is About</h1>
+        <div className='container'>
+            <p className='custom'>
+            Ema-john is guided by four principles: customer obsession rather than competitor focus, passion for invention, commitment to operational excellence, and long-term thinking. Ema-john strives to be Earth’s most customer-centric company, Earth’s best employer, and Earth’s safest place to work. Customer reviews, 1-Click shopping, personalized recommendations, Prime, Fulfillment by Ema-john, AWS, Kindle Direct Publishing, Kindle, Career Choice, Fire tablets, Fire TV, Ema-john Echo, Alexa, Just Walk Out technology, Ema-john Studios, and The Climate Pledge are some of the things pioneered by Ema-john.
+            </p>
         </div>
     );
 };
