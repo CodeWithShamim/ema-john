543620ff745f686125b2c6c388bddbf6650f21d3
codewithshamim <shamimislamonline@gmail.com>
2022-04-02 18:31:08 +0600

    active link added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import logo from '../../images/Logo.svg';
-import './Header.css';
+import React from "react";
+import logo from "../../images/Logo.svg";
+import "./Header.css";
 // FontAwesomeIcon
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import ActiveLink from "../ActiveLink/ActiveLink";
+// import { Link } from "react-router-dom";
 
+const Header = () => {
+  return (
+    <div className="row header-sticky">
+      <nav className="header-nav col-md-12 header-sticky">
+        <ActiveLink to="/">
+          <img src={logo} alt="" />
+        </ActiveLink>
 
-const Header=() => {
-    return (
-        <div className='row header-sticky'>
-            <nav className='header-nav col-md-12 header-sticky'>
-                <Link to='/'><img src={logo} alt="" /></Link>
-
-       
-                <input type="checkbox" id='check'/>
-                <label htmlFor="check" className='barIcon'>
-                    <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
-                </label>
-                <div className='me-5 header-info'>
-                    <Link to="/shop">Shop</Link>
-                    <Link to="/order-review">Order Review</Link>
-                    <Link to="/manage-inventory">Manage Inventory</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/privacy&policy">Privacy & Policy</Link>
-                </div>
-            </nav>
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="barIcon">
+          <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+        </label>
+        <div className="me-5 header-info d-md-flex">
+          <ActiveLink to="/shop">Shop</ActiveLink>
+          <ActiveLink to="/order-review">Order Review</ActiveLink>
+          <ActiveLink to="/manage-inventory">Manage Inventory</ActiveLink>
+          <ActiveLink to="/about">About</ActiveLink>
+          <ActiveLink to="/privacy&policy">Privacy & Policy</ActiveLink>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
