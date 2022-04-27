c92cda401cba59487b2e3eabc1be531bcd69a252
codewithshamim <shamimislamonline@gmail.com>
2022-04-27 15:15:20 +0600

    heroku server link added

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -4,7 +4,7 @@ const useProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
 
-        fetch("http://localhost:5000/product")
+        fetch("https://ema-john-100.herokuapp.com/product")
             // fetch("products.json")
             .then(res => res.json())
             .then(data => setProducts(data));
