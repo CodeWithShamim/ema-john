e12aa49c08c85f4bc1222da11028d252ba4fa96a
codewithshamim <shamimislamonline@gmail.com>
2022-05-10 12:55:45 +0600

    skeleton updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -18,40 +18,31 @@ const Product = ({ product, loading, addToCart }) => {
       // data-aos-duration="700"
       className="product col-md-5 ms-0 ms-md-4"
     >
-      {loading ? (
-        <>
-          <SkeletonTheme color="#202020" highlightColor="#444">
-            <p>
-              <Skeleton
-                height={100}
-                width={100}
-                containerClassName="avatar-skeleton"
-              />
-              <Skeleton count={8} />
-            </p>
-          </SkeletonTheme>
-        </>
-      ) : (
-        <>
-          <div className="text-center">
-            <img src={img} alt="" className="img-fluid rounded" />
-          </div>
-          <div className="py-5">
-            <p className="product-name">{name}</p>
-            <p className="product-price">Price: ${price}</p>
-            <div className="product-info">
-              <p>Seller: {seller}</p>
-              <p>Ratings: {ratings}</p>
-            </div>
-          </div>
-          <button className="cartButton" onClick={() => addToCart(product)}>
-            <p style={{ color: "black", fontWeight: "bold" }}>
-              Add to Cart{" "}
-              {<FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>}
-            </p>
-          </button>
-        </>
-      )}
+      <div className="text-center">
+        {loading ? (
+          <Skeleton width={100} height={100} />
+        ) : (
+          <img src={img} alt="" className="img-fluid rounded" />
+        )}
+      </div>
+      <div className="py-5">
+        <p className="product-name">
+          {!loading ? name : <Skeleton height={30} />}
+        </p>
+        <p className="product-price">
+          {!loading ? `Price: ${price}` : <Skeleton />}
+        </p>
+        <div className="product-info">
+          <p>{!loading ? `Seller: ${seller}` : <Skeleton />}</p>
+          <p>{!loading ? `Ratings: : ${ratings}` : <Skeleton />}</p>
+        </div>
+      </div>
+      <button className="cartButton" onClick={() => addToCart(product)}>
+        <p style={{ color: "black", fontWeight: "bold" }}>
+          Add to Cart{" "}
+          {<FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>}
+        </p>
+      </button>
     </div>
   );
 };
